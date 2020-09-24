9424e6014d6dd0a7ef81c384da472d99c1fbf747
ghsong <ghsong@greenlamp.kr>
2020-09-24 10:47:08 +0900

    Exception 컨트롤러 swagger 파일 생성 ignore

--- src/main/java/com/ghsong/myboard/modules/controller/ExceptionController.java
+++ src/main/java/com/ghsong/myboard/modules/controller/ExceptionController.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+@ApiIgnore
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/exception")
